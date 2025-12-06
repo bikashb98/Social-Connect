a23601e8634107fe8b92531a3baa60124a175fe4
Bikash Roy <64064624+bikashb98@users.noreply.github.com>
2025-12-07 01:12:52 +0530

    Added a password reset link endpoint

---
+++ app/api/auth/password-reset/route.ts
@@ -0,0 +1,38 @@
+import supabase from "@/app/supabaseClient";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const email = body?.email;
+
+  const { data: emailData, error: emailError } = await supabase
+    .from("users")
+    .select("email")
+    .eq("email", email)
+    .maybeSingle();
+
+  if (!emailData || emailError) {
+    return NextResponse.json(
+      { message: "Email not found " + emailError?.message },
+      { status: 404 },
+    );
+  }
+
+  console.log("Email Data:", emailData);
+
+  const { error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: "http://localhost:3000/auth/password-reset-confirm",
+  });
+
+  if (error) {
+    return NextResponse.json(
+      { message: "Error sending password reset email:" + error.message },
+      { status: 500 },
+    );
+  }
+
+  return NextResponse.json(
+    { message: "Password reset email sent" },
+    { status: 200 },
+  );
+}
