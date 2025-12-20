3b53c01e4a2cc2b633fd729f032ea887de1c331e
Bikash Roy <64064624+bikashb98@users.noreply.github.com>
2025-12-21 01:01:23 +0530

    Added automatic profile creation on the verify route

--- app/api/auth/verify/route.ts
+++ app/api/auth/verify/route.ts
@@ -52,6 +52,17 @@ export async function POST(req: Request) {
       );
     }
 
+    const {error: profileError} = await supabase.from("profiles").insert({
+      user_id: user.id,
+    });
+
+    if (profileError) {
+      return new Response(
+        JSON.stringify({ error: "Error creating profile:" + profileError.message }),
+        { status: 500 },
+      );
+    }
+
     return new Response(
       JSON.stringify({ message: "User verified successfully" }),
       { status: 200 },
